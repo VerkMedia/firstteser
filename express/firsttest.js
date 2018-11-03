d3aa7bec4388d5dce0a52730c843674262451917
unknown <josephvarecha@gmail.com>
2018-11-02 22:51:07 -0400

    NEW STUFF

--- express/firsttest.js
+++ express/firsttest.js
@@ -1,19 +1,17 @@
-
-
 var one = 1;
 var two = 2;
 
-
-function addMe(a , b) {
+/* function addMe(a , b) {
      c = a + b;
     return c;
-}
+} */
 
+joe = {
+  name: "Joe",
+  eyes: "blue",
+  age: 29
+};
 
+var addMe = (a, b) => a + b;
 
-
-
-//(a,b) => {a + b};
-
-
-console.log(addMe(one,two));
+console.log(joe.name);
