ec96f07fc8725946de3224b1e32734b0419688e7
unknown <josephvarecha@gmail.com>
2018-11-09 10:32:23 -0500

    second try

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ import data from "./data/DATA.json";
 const app = express();
 const PORT = 3000;
 
+//path /
+app.use(express.static("public"));
+// path /images/
+app.use("/images", express.static("images"));
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.post("/newItem", (req, res) => {
+  res.send(`a post request with /newItem on port ${PORT}`);
+});
+
+app.put("/item", (req, res) => {
+  res.send(`a put request with /item on port ${PORT}`);
+});
+
+app.delete("/item", (req, res) => {
+  res.send(`a delete request with /item on port ${PORT}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Your Server is running on port ${PORT}`);
-  console.log(data);
+  // console.log(data);
 });
